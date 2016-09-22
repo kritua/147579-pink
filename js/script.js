af05b9aef7777b3b58ca133cbfcb51c4c45f318e
Kri Tua <kritua2@gmail.com>
2016-09-23 01:23:29 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -3,20 +3,25 @@ var buttonClose = document.querySelector('.header__button-close');
 var menuBlock = document.querySelector('.header__menu');
 var headerBlock = document.querySelector('.header');
 
-buttonClose.classList.add('hidden');
+window.onload = function() {
+  menuBlock.classList.add('header__menu--hidden');
+  buttonClose.classList.add('header__button--hidden');
+  buttonBurger.classList.remove('header__button--hidden');
+  headerBlock.classList.remove('header--menu-opened');
+};
 
 function openMenu() {
   menuBlock.classList.remove('header__menu--hidden');
   headerBlock.classList.add('header--menu-opened');
-  buttonBurger.classList.add('hidden');
-  buttonClose.classList.remove('hidden');
+  buttonBurger.classList.add('header__button--hidden');
+  buttonClose.classList.remove('header__button--hidden');
 }
 
 function closeMenu() {
   menuBlock.classList.add('header__menu--hidden');
   headerBlock.classList.remove('header--menu-opened');
-  buttonBurger.classList.remove('hidden');
-  buttonClose.classList.add('hidden');
+  buttonBurger.classList.remove('header__button--hidden');
+  buttonClose.classList.add('header__button--hidden');
 }
 
 buttonBurger.addEventListener('click', openMenu);
